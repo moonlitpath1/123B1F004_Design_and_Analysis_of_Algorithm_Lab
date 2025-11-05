0476881af958787d77ba676b4067d77a530402fb
Anushka Badhe <155603722+moonlitpath1@users.noreply.github.com>
2025-11-05 15:59:51 +0530

    update1
    Added problem statement for Merge Sort implementation.

--- assignment1.cpp
+++ assignment1.cpp
@@ -1,3 +1,13 @@
+//Anushka Badhe
+//TY-IT-A
+//123B1F004
+
+/*Problem Statement
+Design and implement a sorting algorithm using Merge Sort to efficiently arrange customer
+orders based on their timestamps. The solution should handle a large dataset (up to 1 million
+orders) with minimal computational overhead. Additionally, analyze the time complexity and
+compare it with traditional sorting techniques.
+*/
 #include <iostream>
 #include <vector>
 #include <string>
